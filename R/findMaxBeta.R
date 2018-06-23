ca2c89554804c94a0c00d3c0f4aff8f0a649b9fc
daniel <caetanods1@gmail.com>
2018-06-23 13:49:49 -0500

    Change function to compute better bounds.

--- R/findMaxBeta.R
+++ R/findMaxBeta.R
@@ -1,11 +1,17 @@
-findMaxBeta <- function(ncats){
-    ## Function to find the max realized value for beta.
-    ## Dependent on the number of categories beta has no effect if larger than a threshould value.
-    beta <- 0
-    gamma.vec <- rep(0, times=ncats)
-    while( gamma.vec[1] < ncats ){
-        beta <- beta+1
-        gamma.vec <- discreteGamma(shape=beta, ncats=ncats)
+findMaxBeta <- function(ncats, precision = 0.01){
+    ## Finds the lower and upper bounds for the beta parameter for the Gamma distribution.
+    ## Prevents the generation of the 0 valued rates.
+    
+    lb <- 0.01 ## This will result in a very low min(rate). This is hard coded.
+    
+    ## The upper bound will depend on the ncat.
+    ub <- lb
+    new <- discreteGamma(shape=ub, ncats=ncats)[ncats]
+    while( new > 0 ){
+        ub <- ub + precision
+        new <- discreteGamma(shape=ub, ncats=ncats)[ncats]
     }
-    return( beta )
+    ub <- ub - precision ## One step back.
+    bound <- setNames(c(lb, ub), c("lower","upper"))
+    return( bound )
 }
